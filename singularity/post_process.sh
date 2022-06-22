4943afb96e791094ce6bd38f5c7d9023e4e3bc31
Stephen Antogiovanni <106194360+santogiovanni@users.noreply.github.com>
2022-06-22 10:39:48 -0400

    Add files via upload

---
+++ singularity/post_process.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+#SBATCH --ntasks=1
+#SBATCH --qos=batch
+#SBATCH --time=48:00:00
+#SBATCH --mem-per-cpu=5G
+#SBATCH --mail-user=hannah.blau@jax.org
+#SBATCH --mail-type=END,FAIL
+
+module load singularity
+
+singularity exec marea_python.sif python ../scripts/post_process.py \
+-p /projects/robinson-lab/marea/pubtator/current -r /projects/robinson-lab/marea/data/pubmed_rel \
+-n /projects/robinson-lab/marea/data/nltk_data -o /projects/robinson-lab/marea/data/pubmed_cr/new
