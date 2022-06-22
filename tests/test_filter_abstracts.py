2d1089a4bee7f08f8565e2491ce7aecf4081ea65
Stephen Antogiovanni <106194360+santogiovanni@users.noreply.github.com>
2022-06-22 10:29:27 -0400

    Add files via upload

---
+++ tests/test_filter_abstracts.py
@@ -0,0 +1,156 @@
+import unittest
+from os import makedirs
+from scripts.filter_abstracts import PMID_INDEX, extract_descriptors, extract_keywords, \
+    find_relevant_abstracts, is_relevant
+from scripts.my_lemmatizer import MyLemmatizer
+from scripts.nlp_utils import nltk_setup
+from scripts.query_mesh import merge_descendants
+
+
+class FilterAbstractsTestCase(unittest.TestCase):
+
+    def test_extract_descriptors(self):
+        descriptor_str = """D000310 Adrenal Gland Neoplasms N | D000311 Adrenal 
+        Glands N | D000328 Adult N | D002404 Catheterization Y | D006801 
+        Humans N | D006977 Hypertension, Renal N | D010690 Phlebography Y | 
+        D012082 Renal Veins Y | D012083 Renin N"""
+        expected = {'D000310': False, 'D000311': False, 'D000328': False,
+                    'D002404': True, 'D006801': False, 'D006977': False,
+                    'D010690': True, 'D012082': True, 'D012083': False}
+        self.assertEqual(expected, extract_descriptors(descriptor_str),
+                         'Extracted descriptors do not match expected.')
+
+    def test_extract_keywords(self):
+        nltk_setup('testdata/nltk_data/')
+        lem = MyLemmatizer()
+        keyword_str = 'Aging-related tau astrogliopathy (ARTAG) Y | Astrocytes N ' +\
+                      '| Atypical Alzheimer disease N | Clinical heterogeneity N | Tau N'
+        expected = {'aging-related tau astrogliopathy ( artag )': True,
+                    'astrocyte': False, 'atypical alzheimer disease': False,
+                    'clinical heterogeneity': False, 'tau': False}
+        self.assertEqual(expected, extract_keywords(lem, keyword_str),
+                         'Extracted keywords do not match expected.')
+
+    def test_is_relevant(self):
+        abs_dict = {'D000855': True, 'D000073496': False, 'D010591': False,
+                    'D003638': False, 'D037061': True}
+        desired0 = {'D000855', 'D010591', 'D020385', 'D060486'}
+        desired1 = {'D010591', 'D020385', 'D060486'}
+        desired2 = {'D020385', 'D060486'}
+        self.assertIs(True, is_relevant(abs_dict, desired0, True),
+                      'Should be relevant with one major topic.')
+        self.assertIs(True, is_relevant(abs_dict, desired0, False),
+                      'Should be relevant with two topics, one major one not.')
+        self.assertIs(False, is_relevant(abs_dict, desired1, True),
+                      'Should be not relevant, no matching major topic.')
+        self.assertIs(True, is_relevant(abs_dict, desired1, False),
+                      'Should be relevant with one topic, not major.')
+        self.assertIs(False, is_relevant(abs_dict, desired2, True),
+                      'Should be not relevant, no overlap of (major) topics.')
+        self.assertIs(False, is_relevant(abs_dict, desired2, False),
+                      'Should be not relevant, no overlap of (any) topics.')
+        search_set = {'neoplasm, breast', 'breast neoplasms', 'breast neoplasm',
+                      'unilateral breast neoplasms', 'breast neoplasm, unilateral',
+                      'breast neoplasms, unilateral', 'breast carcinoma in situ',
+                      'carcinoma, ductal, breast', 'carcinomas, lobular',
+                      'lobular carcinomas', 'lobular carcinoma',
+                      'carcinoma, lobular', 'breast neoplasms, male',
+                      'male breast neoplasm', 'breast neoplasm, male',
+                      'neoplasm, male breast', 'inflammatory breast neoplasms',
+                      'neoplasms, inflammatory breast',
+                      'breast neoplasms, inflammatory',
+                      'breast neoplasm, inflammatory', 'inflammatory breast neoplasm',
+                      'neoplasm, inflammatory breast',
+                      'hereditary breast and ovarian cancer syndrome',
+                      'triple negative breast neoplasms'}
+        kw0 = {'breast carcinoma in situ': False, 'intraductal papilloma': False,
+               'nipple discharge': False}
+        kw1 = {'breast neoplasms': True, 'oestrogen receptors': True,
+               'signal transduction': True}
+        kw2 = {'breast cancer': False, 'proteomic': False,
+               'triorganotin isothiocyanates': False, 'vimentin': False,
+               'itraq': False}
+        kw3 = {'breast cancer': False, 'case report': False,
+               'gastrointestinal metastasis': False,
+               'intussusception': False, 'invasive lobular carcinoma': False}
+        self.assertIs(True, is_relevant(kw0, search_set, False),
+                      "Should be relevant: 'breast carcinoma in situ'.")
+        self.assertIs(True, is_relevant(kw1, search_set, False),
+                      "Should be relevant: 'breast neoplasms'.")
+        self.assertIs(False, is_relevant(kw2, search_set, False),
+                      "Should not be relevant: 'breast cancer' does not match.")
+        self.assertIs(False, is_relevant(kw3, search_set, False),
+                      "Should not be relevant: 'invasive lobular carcinoma' does not match.")
+        self.assertIs(False, is_relevant(kw0, search_set, True),
+                      "Should not be relevant: no major topic keywords.")
+        self.assertIs(True, is_relevant(kw1, search_set, True),
+                      "Should be relevant: 'breast neoplasms' major topic.")
+        self.assertIs(False, is_relevant(kw2, search_set, True),
+                      "Should not be relevant: no major topic keywords.")
+        self.assertIs(False, is_relevant(kw3, search_set, True),
+                      "Should not be relevant: no major topic keywords.")
+
+    def test_find_relevant_abstracts(self):
+        test_filename = 'test_abstracts'
+        input_dir = 'testdata/pubmed_txt/'
+        output_dir = 'testdata/pubmed_rel/'
+        makedirs(output_dir, exist_ok=True)
+        # D005796 = Genes, D009369 = Neoplasms, D037102 = Lectins
+        test_params = [
+            'D005796-D009369-D037102',
+            'D009369-D037102',
+            'D005796',
+            'D009369',
+            'D037102'
+        ]
+        relevant_pmids = {
+            'D005796-D009369-D037102':
+                ['273474', '273475', '273632', '273634', '274699', '274701',
+                 '274703', '274705', '274710', '274713', '274714', '274718',
+                 '274719', '274720', '274721', '31640550', '31652515',
+                 '31721357', '31723456', '31723458', '31723461', '31724699',
+                 '31754330', '31778855'],
+            'D005796-D009369-D037102-m':
+                ['274699', '274703', '274705', '274710', '274714', '274719',
+                 '274720', '274721', '31640550', '31652515', '31721357',
+                 '31723456', '31723458', '31723461', '31724699', '31754330',
+                 '31778855'],
+            'D009369-D037102':
+                ['273474', '273475', '273632', '273634', '274699', '274705',
+                 '274718', '274721', '31640550', '31652515', '31721357',
+                 '31723456', '31723458', '31723461', '31724699'],
+            'D009369-D037102-m':
+                ['274699', '274705', '274721', '31640550', '31652515',
+                 '31721357', '31723456', '31723458', '31723461', '31724699'],
+            'D005796':
+                ['274701', '274703', '274710', '274713', '274714', '274719',
+                 '274720', '31754330', '31778855'],
+            'D005796-m':
+                ['274703', '274710', '274714', '274719', '274720', '31754330',
+                 '31778855'],
+            'D009369':
+                ['273474', '273475', '273632', '273634', '274718',
+                 '31723456', '31723458', '31723461'],
+            'D009369-m': ['31723456', '31723458', '31723461'],
+            'D037102':
+                ['274699', '274705', '274721', '31640550', '31652515',
+                 '31721357', '31724699'],
+            'D037102-m':
+                ['274699', '274705', '274721', '31640550', '31652515',
+                 '31721357', '31724699']
+        }
+        for params in test_params:
+            descriptors = params.split('-')
+            desired = merge_descendants(descriptors)
+            for major in (False, True):
+                pmid_key = params + '-m' if major else params
+                find_relevant_abstracts(input_dir + test_filename + '.txt',
+                                        output_dir, major, desired)
+                with open(output_dir + test_filename + '_rel.tsv') as outfile:
+                    output_pmids = [line.split()[PMID_INDEX] for line in outfile]
+                self.assertEqual(relevant_pmids[pmid_key], output_pmids,
+                                 'Incorrect PMIDs for ' + pmid_key)
+
+
+if __name__ == '__main__':
+    unittest.main()
---
+++ tests/test_text_post_processor.py
@@ -0,0 +1,91 @@
+import unittest
+from scripts.text_post_processor import TextPostProcessor
+
+
+class TextPostProcessorTestCase(unittest.TestCase):
+
+    # @classmethod
+    # def setUpClass(cls):
+    #     tp = TextPostProcessor('testdata/nltk_data/')
+
+    def test_lowercase_first(self):
+        self.assertEqual('rEP-b', TextPostProcessor.lowercase_first('REP-b'))
+        self.assertEqual('bound', TextPostProcessor.lowercase_first('bound'))
+        self.assertEqual('', TextPostProcessor.lowercase_first(''))
+
+    def test_process_phrase(self):
+        tp = TextPostProcessor('testdata/nltk_data/')
+        # a sentence after concept replacement
+        before = ''.join(['The inhibition of binding for  NCBIGene106478911  ',
+                          'and DHT by  NCBIGene106478911 ,  MESHD013739  (T),',
+                          '  MESHD013196 , dehydroepiandosterone (DHEA),  ',
+                          'MESHD019314  ( NCBIGene6822 ),  MESHD000735  (A) ',
+                          'and 5-androstene-3beta, 17beta-diol (Adiol) was ',
+                          'tested with the use of  MESHD003911 -coated charcoal',
+                          ' separation of bound and free  NCBIGene106478911 , ',
+                          'respectively, and  MESHD013196 .'])
+        after = ''.join(['inhibition binding ncbigene106478911 ',
+                         'dht ncbigene106478911 meshd013739',
+                         ' meshd013196 dehydroepiandosterone dhea ',
+                         'meshd019314 ncbigene6822 meshd000735 ',
+                         '5 androstene 3beta 17beta diol adiol ',
+                         'test use meshd003911 coat charcoal ',
+                         'separation bound free ncbigene106478911 ',
+                         'respectively meshd013196'])
+        self.assertEqual(after, tp.process_phrase(before))
+        before = ''.join(['Outcomes of unrelated cord blood transplantation in ',
+                          'pediatric recipients. We report results of unrelated',
+                          ' cord blood transplants (UCBT) in 29 pediatric ',
+                          'recipients in one center and the risk factors ',
+                          'associated with survival. Median age: 9 years ',
+                          '(0.5-20); diagnosis: ALL (9),  MESHD015470  (4),  ',
+                          'MESHD015464  (1),  MESHD006816  (3), HLH (1),  ',
+                          'MESHD008228  (3),  NCBIGene9253  (2); B-thal (1), ',
+                          'FA (1), FEL (1), Krabbe (1), WAS (1), SAA (1).'])
+        after = ''.join(['outcome unrelated cord blood transplantation ',
+                         'pediatric recipient report result unrelated',
+                         ' cord blood transplant ucbt 29 pediatric ',
+                         'recipient one center risk factor ',
+                         'associate survival median age 9 year ',
+                         '0 5 20 diagnosis all 9 meshd015470 4 ',
+                         'meshd015464 1 meshd006816 3 hlh 1 ',
+                         'meshd008228 3 ncbigene9253 2 thal 1 ',
+                         'fa 1 fel 1 krabbe 1 was 1 saa 1'])
+        self.assertEqual(after, tp.process_phrase(before))
+        before = ''.join(["A Neurologist's Perspective on Thymectomy for  ",
+                          'MESHD009157 : Current Perspective and Future Trials'
+                          '. The first randomized blinded study of thymectomy '
+                          'in  MESHD009157  was designed to answer 3 '
+                          'questions.'])
+        after = ''.join(["neurologist perspective thymectomy ",
+                         'meshd009157 current perspective future trial '
+                         'first randomize blind study thymectomy '
+                         'meshd009157 design answer 3 '
+                         'question'])
+        self.assertEqual(after, tp.process_phrase(before))
+        before = ''.join(['A side effect of  NCBIGene1440 -mild  MESHD010146',
+                          ' -was observed in one patient, but it was assess ',
+                          'tolerable.'])
+        after = ''.join(['side effect ncbigene1440 mild meshd010146',
+                         ' observe one patient ass ',
+                         'tolerable'])
+        self.assertEqual(after, tp.process_phrase(before))
+        before = ''.join(['RESULTS: High levels of  NCBIGene4137 -A and  ',
+                          'NCBIGene4137 -C (above the median) in blood were ',
+                          'associated with lower risk of  MESHD003704  and  '
+                          'MESHD000544  ( NCBIGene4137 -A:  MESHD003704 [95% ',
+                          'CI] = 0.85[0.70-1.04];  MESHD000544  0.71[0.52-0.98]',
+                          ' and  NCBIGene4137 -C: Dementia 0.84[0.70-1.00];  ',
+                          'MESHD000544  0.78[0.60-1.03]).'])
+        after = ''.join(['results high level ncbigene4137 ',
+                         'ncbigene4137 median blood ',
+                         'associate low risk meshd003704 '
+                         'meshd000544 ncbigene4137 meshd003704 95 ',
+                         'ci 0 85 0 70 1 04 meshd000544 0 71 0 52 0 98',
+                         ' ncbigene4137 dementia 0 84 0 70 1 00 ',
+                         'meshd000544 0 78 0 60 1 03'])
+        self.assertEqual(after, tp.process_phrase(before))
+
+
+if __name__ == '__main__':
+    unittest.main()
